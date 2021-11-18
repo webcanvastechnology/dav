d41e4f65001423afe36279bd5afce2ec8489003d
unknown <tanmoy.bishoi@gmail.com>
2021-11-18 20:51:47 +0530

    update toastr2

--- config/app.php
+++ config/app.php
@@ -232,7 +232,7 @@ return [
         'View' => Illuminate\Support\Facades\View::class,
         'Form' => Collective\Html\FormFacade::class,
       'Html' => Collective\Html\HtmlFacade::class,
-
+      'Toastr'  => Brian2694\Toastr\Facades\Toastr::class,
     ],
 
 ];
